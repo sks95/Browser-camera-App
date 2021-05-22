20cd3d3bffecc0838cb8560cbdab613094fd88fe
SHIVAM <yyyshivamyyy@gmail.com>
2021-05-23 02:43:06 +0530

    :books: added record and filter functionality

--- main_activity/script.js
+++ main_activity/script.js
@@ -1,4 +1,10 @@
 let videoRecorder = document.querySelector("#record-video");
+let capturebtn = document.querySelector("#capture");
+let timingELem = document.querySelector("#timing");
+let allFilters = document.querySelectorAll(".filter");
+let clearObj;
+let uiFilter = document.querySelector(".ui-filter");
+let filterColor = "";
 let constraints = {
     video: true,
     audio: true
@@ -31,7 +37,7 @@ navigator.mediaDevices
             // url 
             a.href = url;
             a.click();
-            buffer=[];
+            buffer = [];
         })
     }
     ).catch(function (err) {
@@ -44,11 +50,75 @@ videoRecorder.addEventListener("click", function () {
     }
     if (recordState == false) {
         mediaRecorder.start();
-        videoRecorder.innerHTML = "Recording....";
+        // videoRecorder.innerHTML = "Recording....";
+        videoRecorder.classList.add("record-animation");
+        startCounting();
         recordState = true;
     } else {
         mediaRecorder.stop();
-        videoRecorder.innerHTML = "Record";
+        videoRecorder.classList.remove("record-animation");
+        stopCounting();
         recordState = false;
     }
 })
+capturebtn.addEventListener("click", function () {
+    // create a canvas element
+    // equal to your video frame
+    let canvas = document.createElement("canvas");
+    canvas.width = videoElem.videoWidth;
+    canvas.height = videoElem.videoHeight;
+    let tool = canvas.getContext("2d");
+    capturebtn.classList.add("capture-animation");
+    // draw a frame on that canvas
+    tool.drawImage(videoElem, 0, 0);
+    // translucent 
+    tool.fillStyle = filterColor;
+    tool.fillRect(0, 0, canvas.width, canvas.height);
+    // above layer things are drawn
+    // toDataUrl 
+    let link = canvas.toDataURL();
+    // download 
+    let anchor = document.createElement("a");
+    anchor.href = link;
+    anchor.download = "file.png";
+    anchor.click();
+    anchor.remove();
+    canvas.remove();
+    // i need one secomd of that animation
+    setTimeout(function () {
+        capturebtn.classList.remove("capture-animation");
+    }, 1000)
+})
+function startCounting() {
+    timingELem.classList.add("timing-active");
+    let timeCount = 0;
+    clearObj = setInterval(function () {
+        let seconds = (timeCount % 60) < 10 ? `0${timeCount % 60}` : `${timeCount % 60}`;
+        let minutes = (timeCount / 60) < 10 ? `0${Number.parseInt(timeCount / 60)}` : `${Number.parseInt(timeCount / 60)}`;
+        let hours = (timeCount / 3600) < 10 ? `0${Number.parseInt(timeCount / 3600)}` : `${Number.parseInt(timeCount / 3600)}`;
+        timingELem.innerText = `${hours}:${minutes}:${seconds}`;
+        timeCount++;
+    }, 1000);
+}
+function stopCounting() {
+    timingELem.classList.remove("timing-active");
+    timingELem.innerText = "00: 00: 00";
+    clearInterval(clearObj);
+}
+// filter apply
+for (let i = 0; i < allFilters.length; i++) {
+    allFilters[i].addEventListener("click", function () {
+        // add filter to ui
+        let color = allFilters[i].style.backgroundColor
+        if (color) {
+            uiFilter.classList.add("ui-filter-active");
+            uiFilter.style.backgroundColor = color;
+            filterColor = color;
+        } else {
+            uiFilter.classList.remove("ui-filter-active");
+            uiFilter.style.backgroundColor = "";
+            filterColor = "";
+
+        }
+    })
+}
